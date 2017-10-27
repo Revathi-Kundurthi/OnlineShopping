13b75c8de57a7b15fc7c5fef243bce9209dbf666
Revathi Kundurthi <revathi.kundurthi@gmail.com>
2017-10-27 17:31:25 -0400

    My Project Setup completed Successfully

---
+++ ShoppingBackend/src/main/java/net/Icon/ShoppingBackend/App.java
@@ -0,0 +1,13 @@
+package net.Icon.ShoppingBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
